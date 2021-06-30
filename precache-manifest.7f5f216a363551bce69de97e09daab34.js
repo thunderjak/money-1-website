6b2ff7dc3e71a2805fcaa60d4fa6a55a05f15b8e
thunderjak <1026253290@qq.com>
2021-06-30 22:33:46 +0800

    deploy

---
+++ precache-manifest.7f5f216a363551bce69de97e09daab34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb2fef68be537788c7b6",
+    "url": "/money-1-website/css/app.03189ab2.css"
+  },
+  {
+    "revision": "f7ab75d0475a7cd46bd8bc4cccc3e210",
+    "url": "/money-1-website/index.html"
+  },
+  {
+    "revision": "bb2fef68be537788c7b6",
+    "url": "/money-1-website/js/app.1de13f2b.js"
+  },
+  {
+    "revision": "087fd9eb5dc33fb53df2",
+    "url": "/money-1-website/js/chunk-vendors.acdcbe66.js"
+  },
+  {
+    "revision": "25a7e11dfe5c86ca6e0188a145c795e8",
+    "url": "/money-1-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-1-website/robots.txt"
+  }
+]);
